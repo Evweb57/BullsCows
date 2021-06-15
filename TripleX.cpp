60ce1c2e8f3fe0f972f06f28c78a820c25532f8f
Evweb57 <ericvanmeter57@gmail.com>
2021-06-14 22:06:08 -0500

    Update TripleX.cpp
    Added iostream and basic terminal prompts.

--- TripleX.cpp
+++ TripleX.cpp
@@ -2,5 +2,8 @@
 
 int main()
 {
+    std::cout << "You're a secret agent attempting to break into the server!";
+    std::cout << std::endl;
+    std::cout << "Enter the correct code to begin...";
     return 0;
 }
